8f9264efe2b4511d99b29d7b7f75a7cbb03d1d9e
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-09-09 17:50:15 +0200

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+
+const router = require('./router');
+
+app.use(bodyParser());
+app.use(router.routes());
+
+const port = 3003;
+
+app.listen(port, () => console.log(`Server listening on localhost:${port}`));
