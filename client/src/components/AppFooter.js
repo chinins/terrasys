a3e2276d8dac2861bbea4c001640a30a4b02ab85
chinins <34057551+chinins@users.noreply.github.com>
2018-10-24 15:41:41 +0200

    Update AppFooter.js

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class AppFooter extends Component {
   render () {
     return (
-      <div className="app-footer">2018, created by Olga Chinina & Daniel Brain</div>
+      <div className="app-footer">2018, created by Olga Chinina</div>
     )
   }
 }
